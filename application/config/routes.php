ed2e6a3ab3808957507cba52fd0a4a43389e4a17
fannyhasbi <fnyhsbi@gmail.com>
2020-10-17 23:45:27 +0700

    remove routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,9 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$route['product'] = 'main/product';
-$route['product/(:num)'] = 'main/productDetail/$1';
-
 $route['default_controller'] = 'main';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
